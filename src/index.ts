987a10585cd1edfa877c9572784ace46ecd44a39
Tu Tran <dp0613@yandex.com>
2022-04-26 16:34:07 +0700

    v1.4.1

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { IApi, BundlerConfigType } from 'umi';
+import { ESBuildPlugin, ESBuildMinifyPlugin } from 'esbuild-loader';
+
+export default (api: IApi) => {
+  api.describe({
+    key: 'esbuild',
+    config: {
+      schema(joi) {
+        return joi.object({
+          target: joi.alternatives(
+            joi.string(),
+            joi.array().items(joi.string()),
+          ),
+          minify: joi.alternatives(
+            joi.boolean(),
+          ),
+        });
+      },
+    },
+    enableBy: api.EnableBy.config,
+  });
+
+  api.modifyBundleConfig((memo, { type }) => {
+    if (memo.optimization) {
+      const { target = 'es2015' } = api.config.esbuild || {};
+      const { minify = true } = api.config.esbuild || {};
+      const optsMap = {
+        [BundlerConfigType.csr]: {
+          minify,
+          target,
+        },
+        [BundlerConfigType.ssr]: {
+          target: 'node10',
+          minify
+        },
+      };
+      const opts = optsMap[type] || optsMap[BundlerConfigType.csr];
+      memo.optimization.minimize = true;
+      memo.optimization.minimizer = [new ESBuildMinifyPlugin(opts)];
+    }
+    if (memo.plugins) {
+      memo.plugins.push(new ESBuildPlugin());
+    }
+    return memo;
+  });
+};
